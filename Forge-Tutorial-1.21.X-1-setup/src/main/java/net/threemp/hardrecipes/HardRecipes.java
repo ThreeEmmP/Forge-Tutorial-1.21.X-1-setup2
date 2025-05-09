7c4610e5675e6f3aa61d9c186b67e532ddd4d737
ThreeEmmP <throwbackwii2006@gmail.com>
2025-05-09 19:32:32 -0400

    Update

--- Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/HardRecipes.java
+++ Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/HardRecipes.java
@@ -1,24 +1,18 @@
 package net.threemp.hardrecipes;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.threemp.hardrecipes.item.ModCreativeModeTabs;
 import net.threemp.hardrecipes.item.ModItems;
 import org.slf4j.Logger;
 
-// Very important Comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(HardRecipes.MOD_ID)
 public class HardRecipes {
@@ -43,27 +37,11 @@ public class HardRecipes {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)  {
+    private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        }
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
     }
 }
