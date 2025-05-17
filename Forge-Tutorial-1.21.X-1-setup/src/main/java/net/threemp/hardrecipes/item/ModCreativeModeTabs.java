d526d5d171404bf308873ae1230fd23209c71edd
ThreeEmmP <throwbackwii2006@gmail.com>
2025-05-17 10:27:48 -0400

    Up to wooden pickaxe in progression

--- Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/item/ModCreativeModeTabs.java
+++ Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/item/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -30,11 +31,53 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.CANDELILLA_WAX.get());
 
                         output.accept(ModItems.WEAK_TOOL_ROD.get());
+                        output.accept(ModItems.STURDY_TOOL_ROD.get());
+                        output.accept(ModItems.STRONG_TOOL_ROD.get());
                         output.accept(ModItems.WEAK_HILT.get());
+                        output.accept(ModItems.STURDY_HILT.get());
+                        output.accept(ModItems.STRONG_HILT.get());
+
                         output.accept(ModItems.WOODEN_BINDING.get());
+                        output.accept(ModItems.STONE_BINDING.get());
+                        output.accept(ModItems.IRON_BINDING.get());
+                        output.accept(ModItems.GOLDEN_BINDING.get());
+                        output.accept(ModItems.DIAMOND_BINDING.get());
                         output.accept(ModItems.WOODEN_PICKEND.get());
+                        output.accept(ModItems.STONE_PICKEND.get());
+                        output.accept(ModItems.IRON_PICKEND.get());
+                        output.accept(ModItems.GOLDEN_PICKEND.get());
+                        output.accept(ModItems.DIAMOND_PICKEND.get());
                         output.accept(ModItems.WOODEN_AXE_HEAD.get());
+                        output.accept(ModItems.STONE_AXE_HEAD.get());
+                        output.accept(ModItems.IRON_AXE_HEAD.get());
+                        output.accept(ModItems.GOLDEN_AXE_HEAD.get());
+                        output.accept(ModItems.DIAMOND_AXE_HEAD.get());
                         output.accept(ModItems.WOODEN_BLADE.get());
+                        output.accept(ModItems.STONE_BLADE.get());
+                        output.accept(ModItems.IRON_BLADE.get());
+                        output.accept(ModItems.GOLDEN_BLADE.get());
+                        output.accept(ModItems.DIAMOND_BLADE.get());
+                        output.accept(ModItems.WOODEN_SPADE.get());
+                        output.accept(ModItems.STONE_SPADE.get());
+                        output.accept(ModItems.IRON_SPADE.get());
+                        output.accept(ModItems.GOLDEN_SPADE.get());
+                        output.accept(ModItems.DIAMOND_SPADE.get());
+
+                        output.accept(ModItems.IRON_BOLT.get());
+                        output.accept(ModItems.IRON_ROD.get());
+
+
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> TO_BE_DEPRECATED = CREATIVE_MODE_TABS.register("to_be_deprecated",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.BARRIER))
+                    .title(Component.translatable("creativetab.hardrecipes.to_be_deprecated"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.PLACEMAT.get());
+
+
                     })
                     .build());
 
--- Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/item/ModItems.java
+++ Forge-Tutorial-1.21.X-1-setup/src/main/java/net/threemp/hardrecipes/item/ModItems.java
@@ -32,8 +32,18 @@ public class ModItems {
 
     public static final RegistryObject<Item> WEAK_TOOL_ROD = ITEMS.register("weak_tool_rod",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STURDY_TOOL_ROD = ITEMS.register("sturdy_tool_rod",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STRONG_TOOL_ROD = ITEMS.register("strong_tool_rod",
+            () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> WEAK_HILT = ITEMS.register("weak_hilt",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STURDY_HILT = ITEMS.register("sturdy_hilt",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STRONG_HILT = ITEMS.register("strong_hilt",
+            () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> WOODEN_BINDING = ITEMS.register("wooden_binding",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WOODEN_PICKEND = ITEMS.register("wooden_pickend",
@@ -42,6 +52,58 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WOODEN_BLADE = ITEMS.register("wooden_blade",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WOODEN_SPADE = ITEMS.register("wooden_spade",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> STONE_BINDING = ITEMS.register("stone_binding",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STONE_PICKEND = ITEMS.register("stone_pickend",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STONE_AXE_HEAD = ITEMS.register("stone_axe_head",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STONE_BLADE = ITEMS.register("stone_blade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> STONE_SPADE = ITEMS.register("stone_spade",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GOLDEN_BINDING = ITEMS.register("golden_binding",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GOLDEN_PICKEND = ITEMS.register("golden_pickend",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GOLDEN_AXE_HEAD = ITEMS.register("golden_axe_head",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GOLDEN_BLADE = ITEMS.register("golden_blade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GOLDEN_SPADE = ITEMS.register("golden_spade",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> IRON_BINDING = ITEMS.register("iron_binding",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_PICKEND = ITEMS.register("iron_pickend",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_AXE_HEAD = ITEMS.register("iron_axe_head",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_BLADE = ITEMS.register("iron_blade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_SPADE = ITEMS.register("iron_spade",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DIAMOND_BINDING = ITEMS.register("diamond_binding",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_PICKEND = ITEMS.register("diamond_pickend",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_AXE_HEAD = ITEMS.register("diamond_axe_head",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_BLADE = ITEMS.register("diamond_blade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> DIAMOND_SPADE = ITEMS.register("diamond_spade",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> IRON_BOLT = ITEMS.register("iron_bolt",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> IRON_ROD = ITEMS.register("iron_rod",
+            () -> new Item(new Item.Properties()));
+
 
 
     public static void register(IEventBus eventBus) {
